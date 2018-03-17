1ddb0ef14107a559fb24fc4903ed926d69a4f8c0
Avinash Barfa <avibarfa98@gmail.com>
2018-03-17 23:30:52 +0530

    Latest Files.

---
+++ app/src/main/java/com/avinashbarfa/homemade/LoginActivity.java
@@ -0,0 +1,96 @@
+package com.avinashbarfa.homemade;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private final static int REQ_CODE = 999;
+    Button btnPhone,btnEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnPhone = (Button)findViewById(R.id.phoneNumber);
+        btnEmail = (Button)findViewById(R.id.emailId);
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLoginPage(LoginType.EMAIL);
+            }
+        });
+        btnPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLoginPage(LoginType.PHONE);
+            }
+        });
+    }
+
+    private void startLoginPage(LoginType loginType) {
+        if (loginType == LoginType.EMAIL) {
+            Intent intent = new Intent(this, AccountKitActivity.class);
+            AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                    new AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.EMAIL, AccountKitActivity.ResponseType.CODE);
+            //use token when "Enable Client Access Token Flow" is yes
+            intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,configurationBuilder.build());
+            startActivityForResult(intent,REQ_CODE);
+        }
+        else if (loginType == LoginType.PHONE) {
+            Intent intent = new Intent(this, AccountKitActivity.class);
+            AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                    new AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.PHONE, AccountKitActivity.ResponseType.CODE);
+            //use token when "Enable Client Access Token Flow" is yes
+            intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,configurationBuilder.build());
+            startActivityForResult(intent,REQ_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQ_CODE){
+            AccountKitLoginResult result = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            if (result.getError() != null) {
+                Toast.makeText(this, ""+result.getError().getErrorType().getMessage() , Toast.LENGTH_SHORT).show();
+                return;
+            }
+            else if (result.wasCancelled()) {
+                Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            else {
+                if (result.getAccessToken() != null)
+                    Toast.makeText(this, "Success ! %s"+result.getAccessToken().getAccountId() , Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(this, "Success ! %s"+result.getAuthorizationCode().substring(0,10) , Toast.LENGTH_SHORT).show();
+
+                startActivity(new Intent(this , MainActivity.class));
+            }
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/avinashbarfa/homemade/MainActivity.java
@@ -0,0 +1,59 @@
+package com.avinashbarfa.homemade;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.facebook.accountkit.AccountKitLoginResult;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnSignOut = (Button)findViewById(R.id.btnLogOut);
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AccountKit.logOut();
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+            @Override
+            public void onSuccess(Account account) {
+                TextView userid , phone ,email;
+
+            /*    userid = (TextView)findViewById(R.id.editUserId);
+                userid.setText(account.getId());
+                phone = (TextView) findViewById(R.id.editPhoneNumber);
+                phone.setText(account.getPhoneNumber().toString());
+                email = (TextView)findViewById(R.id.editEmailId);
+
+                email.setText(account.getEmail());
+            */}
+
+            @Override
+            public void onError(AccountKitError accountKitError) {
+
+            }
+        });
+    }
+
+}
